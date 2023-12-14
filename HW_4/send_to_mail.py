589140978a5c3a57e7a4cd2f8bfa0ce2530b7479
Mike-P63 <rumaish@rambler.ru>
2023-12-14 09:56:49 +0400

    added HW 4 files

---
+++ HW_4/send_to_mail.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from os.path import basename
+
+def sendemail():
+    fromaddr = "mike6371@mail.ru"
+    toaddr = "mike6371@mail.ru"
+    mypass = "DAg8s2cPkTeLqLJQUnrC"
+    reportname = "log.txt"
+
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Привет от питона"
+    text = "Hello"
+
+    msg.attach(MIMEText(text))
+
+    with open(reportname, "rb") as f:
+        part = MIMEApplication(f.read(), Name=basename(reportname))
+        part['Content-Disposition'] = f'attachment; filename="{basename(reportname)}"'
+        msg.attach(part)
+
+    # body = "Это пробное сообщение"
+    # msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+    server.login(fromaddr, mypass)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+if __name__ == '__main__':
+    sendemail()
